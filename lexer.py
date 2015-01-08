d55bf29b853ab520034436c0610019f5e0cc8c96
Bunedri Vivek <buvivek@csews9.cse.iitk.ac.in>
2015-01-08 17:19:27 +0530

    made changes like adding some keywords and tokens

--- lexer.py
+++ lexer.py
@@ -3,33 +3,41 @@
 
 import sys,ply.lex
 
+keywords = {'int':'TYPE_INT','float':'TYPE_FLOAT','double':'TYPE_DOUBLE','for':'KEYW_FOR','if':'KEYW_IF','else':'KEYW_ELSE','return':'KEYW_RETURN','break':'KEYW_BREAK','continue':'KEYW_CONTINUE','class':'KEYW_CLASS_HEADER','def':'KEYW_FUNCTION_HEADER','println':'KEYW_PRINT_LINE',}
 
 
-
-keywords = {'if':'IF','else':'ELSE','return':'RETURN'}
-
-
-tokens = ['EQUALS','REALNUMBER','INT_CONST','HEXANUM','EXPONENTIALNUM','ADDITION','SUBTRACTION','MULTIPLICATION','DIVISION','POWER','EXPONENTIAL','PARENL','PARENR','SEMICOLON','IDENTIFIER','BLOCK_BEGIN','BLOCK_END',]+list(keywords.values())
+tokens =['GREATER_THAN_EQUAL','GREATER_THAN','LESS_THAN_EQUAL','LESS_THAN','NEGATION','AND','OR','NOT_EQUAL','ASSIGNMENT','EQUAL','REALNUMBER','INT_CONST','HEXANUM','EXPONENTIALNUM','ADDITION','SUBTRACTION','MULTIPLICATION','DIVISION','POWER','EXPONENTIAL','PARENL','PARENR','SEMICOLON','IDENTIFIER','BLOCK_BEGIN','BLOCK_END',]+list(keywords.values())
 
 
 
 t_ignore		= ' \t\n'
 t_REALNUMBER		= r'(\d*\.\d+)|(\d+\.\d*)'
 t_INT_CONST		= r'\d+'
-#t_HEXANUM		= r'0[xX][0-9a-fA-F]+'
-#t_EXPONENTIALNUM	= r'((\d*\.\d+)|(\d+\.\d*)|\d+)(e|E)(\+|-)?\d+'
-#t_ADDITION		= r'\+'
-#t_SUBTRACTION		= r'-'
-#t_MULTIPLICATION	= r'\*'
-#t_DIVISION		= r'/'
-#t_POWER		= r'\^'
-#t_EXPONENTIAL		= r'exp'
+t_HEXANUM		= r'0[xX][0-9a-fA-F]+'
+t_EXPONENTIALNUM	= r'((\d*\.\d+)|(\d+\.\d*)|\d+)(e|E)(\+|-)?\d+'
+t_ADDITION		= r'\+'
+t_SUBTRACTION		= r'-'
+t_MULTIPLICATION	= r'\*'
+t_DIVISION		= r'/'
+t_POWER		        = r'\^'
+t_EXPONENTIAL		= r'exp'
 t_PARENL		= r'\('
 t_PARENR		= r'\)'
 t_SEMICOLON             = r'\;'
 t_BLOCK_BEGIN   	= r'\{'
 t_BLOCK_END 		= r'\}'
-t_EQUALS  		= r'=='
+t_EQUAL  		= r'=='
+t_ASSIGNMENT            = r'='
+t_GREATER_THAN_EQUAL    = r'>='
+t_GREATER_THAN          = r'>'
+t_LESS_THAN_EQUAL       = r'<='
+t_LESS_THAN		= r'<'
+t_NEGATION		= r'!'
+t_AND                   = r'&&'
+t_OR			= r'\|\|'
+t_NOT_EQUAL		= r'!='
+
+
 
 
 
@@ -51,10 +59,10 @@ def lex_output(raw_input):
     data = open(raw_input).read()
     lexer.input(data)
 
-    fo = open("foo.txt","w")
+    fo = open("raw_input","rw+")
 
     for tok in lexer:
-	fo.write('('+tok.value+'\t\t\t\t\t"'+tok.type+'")\n')
+        print(''+tok.value+'\t\t\t\t\t"'+tok.type+'"\n')
 
     fo.close()
 
