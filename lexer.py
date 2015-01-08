12fb29df67a6b01e6979f214ba846589f6f8b183
Bunedri Vivek <buvivek@csews9.cse.iitk.ac.in>
2015-01-08 15:43:45 +0530

    file i/o has been added to lexer.py

--- lexer.py
+++ lexer.py
@@ -3,21 +3,26 @@
 
 import sys,ply.lex
 
-tokens = ('REALNUMBER','INTEGERNUMBER','HEXANUM','EXPONENTIALNUM','ADDITION','SUBTRACTION','MULTIPLICATION','DIVISION','POWER','EXPONENTIAL','PARENL','PARENR',)
+tokens = ('REALNUMBER','INT_CONST','HEXANUM','EXPONENTIALNUM','ADDITION','SUBTRACTION','MULTIPLICATION','DIVISION','POWER','EXPONENTIAL','PARENL','PARENR','SEMICOLON','BLOCK_BEGIN','BLOCK_END',)
 
-t_ignore			= ' \t'
+t_ignore		= ' \t\n'
 t_REALNUMBER		= r'(\d*\.\d+)|(\d+\.\d*)'
-t_INTEGERNUMBER		= r'\d+'
-t_HEXANUM			= r'0[xX][0-9a-fA-F]+'
-t_EXPONENTIALNUM	= r'((\d*\.\d+)|(\d+\.\d*)|\d+)(e|E)(\+|-)?\d+'
-t_ADDITION			= r'\+'
-t_SUBTRACTION		= r'-'
-t_MULTIPLICATION	= r'\*'
-t_DIVISION			= r'/'
-t_POWER				= r'\^'
-t_EXPONENTIAL		= r'exp'
-t_PARENL			= r'\('
-t_PARENR			= r'\)'
+t_INT_CONST		= r'\d+'
+#t_HEXANUM		= r'0[xX][0-9a-fA-F]+'
+#t_EXPONENTIALNUM	= r'((\d*\.\d+)|(\d+\.\d*)|\d+)(e|E)(\+|-)?\d+'
+#t_ADDITION		= r'\+'
+#t_SUBTRACTION		= r'-'
+#t_MULTIPLICATION	= r'\*'
+#t_DIVISION		= r'/'
+#t_POWER		= r'\^'
+#t_EXPONENTIAL		= r'exp'
+t_PARENL		= r'\('
+t_PARENR		= r'\)'
+t_SEMICOLON             = r'\;'
+t_BLOCK_BEGIN   	= r'\{'
+t_BLOCK_END 		= r'\}'
+
+
 
 def t_error(t):
     print "Unrecognized char skipped : '%s'" % t.value[0]
@@ -25,8 +30,21 @@ def t_error(t):
 
 lexer = ply.lex.lex()
 
-data = raw_input('Enter expression to evaluate: ')
-lexer.input(data)
+#data = raw_input('Enter expression to evaluate: ')
+#lexer.input(data)
 
-for tok in lexer:
-    print ('('+tok.type+', "'+tok.value+'")')
+def lex_output(raw_input):
+    data = open(raw_input).read()
+    lexer.input(data)
+
+    for tok in lexer:
+        # Open a file
+	fo = open("foo.txt", "a")
+	fo.write('('+tok.value+'\t\t\t\t\t"'+tok.type+'")\n')
+	# Close opened file
+	fo.close()
+
+if __name__ == "__main__":
+	from sys import argv
+	filename, raw_input = argv
+	lex_output(raw_input)
