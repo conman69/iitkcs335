37e59f661e33cb2056876d3bc0f18a98e299659e
Bunedri Vivek <buvivek@csews9.cse.iitk.ac.in>
2015-01-27 14:40:56 +0530

    added test files, made changes to lexer,py

--- lexer.py
+++ lexer.py
@@ -3,14 +3,15 @@
 
 import sys,ply.lex
 
-keywords = {'int':'TYPE_INT','float':'TYPE_FLOAT','double':'TYPE_DOUBLE','for':'KEYW_FOR','if':'KEYW_IF','else':'KEYW_ELSE','return':'KEYW_RETURN','break':'KEYW_BREAK','continue':'KEYW_CONTINUE','class':'KEYW_CLASS_HEADER','def':'KEYW_FUNCTION_HEADER','println':'KEYW_PRINT_LINE',}
+keywords = {'args':'ARGUMENTS','static':'FUNC_TYPE','void':'FUNC_TYPE','package':'KEYW_PACKAGE','int':'TYPE_INT','float':'TYPE_FLOAT','double':'TYPE_DOUBLE','for':'KEYW_FOR','if':'KEYW_IF','else':'KEYW_ELSE','return':'KEYW_RETURN','break':'KEYW_BREAK','continue':'KEYW_CONTINUE','class':'KEYW_CLASS_HEADER','def':'KEYW_FUNCTION_HEADER','println':'KEYW_PRINT_LINE',}
 
 
-tokens =['GREATER_THAN_EQUAL','GREATER_THAN','LESS_THAN_EQUAL','LESS_THAN','NEGATION','AND','OR','NOT_EQUAL','ASSIGNMENT','EQUAL','REALNUMBER','INT_CONST','HEXANUM','EXPONENTIALNUM','ADDITION','SUBTRACTION','MULTIPLICATION','DIVISION','POWER','EXPONENTIAL','PARENL','PARENR','SEMICOLON','IDENTIFIER','BLOCK_BEGIN','BLOCK_END',]+list(keywords.values())
+tokens =['STRING','NEW_LINE','SINGLE_QUOTE','DOUBLE_QUOTE','GREATER_THAN_EQUAL','GREATER_THAN','LESS_THAN_EQUAL','LESS_THAN','NEGATION','AND','OR','NOT_EQUAL','ASSIGNMENT','EQUAL','REALNUMBER','INT_CONST','HEXANUM','EXPONENTIALNUM','ADDITION','SUBTRACTION','MULTIPLICATION','DIVISION','POWER','EXPONENTIAL','PARENL','PARENR','SEMICOLON','IDENTIFIER','BLOCK_BEGIN','BLOCK_END',]+list(keywords.values())
 
 
 
-t_ignore		= ' \t\n'
+t_ignore		= ' \t'
+t_ignore_COMMENT        = r"//[^\n]+|"r"/\*[^(\*/)]+(\*/)"
 t_REALNUMBER		= r'(\d*\.\d+)|(\d+\.\d*)'
 t_INT_CONST		= r'\d+'
 t_HEXANUM		= r'0[xX][0-9a-fA-F]+'
@@ -36,6 +37,10 @@ t_NEGATION		= r'!'
 t_AND                   = r'&&'
 t_OR			= r'\|\|'
 t_NOT_EQUAL		= r'!='
+t_DOUBLE_QUOTE		= r'\"'
+t_SINGLE_QUOTE		= r'\''
+t_NEW_LINE		= r'\n'
+
 
 
 
@@ -50,23 +55,31 @@ def t_IDENTIFIER(t):
     t.type = keywords.get(t.value,'IDENTIFIER')
     return t
 
+def t_STRING(t):
+    r"(?P<start>\"|')[^\"']*(?P=start)"
+    t.value = t.value.replace("\"", "").replace("'", "")
+    return t
+
 lexer = ply.lex.lex()
 
 #data = raw_input('Enter expression to evaluate: ')
 #lexer.input(data)
 
-def lex_output(raw_input):
-    data = open(raw_input).read()
+def lex_output(test_input1):
+    data = open(test_input1).read()
     lexer.input(data)
 
-    fo = open("raw_input","rw+")
+    fo = open("test_input1","rw+")
 
     for tok in lexer:
-        print(''+tok.value+'\t\t\t\t\t"'+tok.type+'"\n')
-
+	if(tok.value == '\n'):
+		print('\n')
+        else:
+         	print(''+tok.value+'\t\t\t//'+tok.type+'\t')
+	
     fo.close()
 
 if __name__ == "__main__":
 	from sys import argv
-	filename, raw_input = argv
-	lex_output(raw_input)
+	filename, test_input1 = argv
+	lex_output(test_input1)
